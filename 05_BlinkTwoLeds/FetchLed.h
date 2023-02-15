d3a5d643b57163d3c0fb484501fcaac3086b89bc
KNikolaich <kirill.nikolaevich@gmail.com>
2023-02-15 12:56:07 +0300

    Отлично, теперь управляем двумя светодиодами. Один на борту, другой на GPIO 4 Один реверсивный, другой нормальный.

--- 05_BlinkTwoLeds/FetchLed.h
+++ 05_BlinkTwoLeds/FetchLed.h
@@ -2,27 +2,40 @@
 // Adapt to the actual board you use if not declared LED_PIN_4.
 #define LED_PIN_4   4
 
-bool LED_ACTIVELEVEL = false;
 // Define a path to the custom web page for the LED blink control.
 #define LED_ENDPOINT  "/led"
-
+// здесь локализованные надписи на кнопках
+#define ON "Включить"
+#define OFF "Выключить"
 const char LED_ONOFF[] PROGMEM = R"(
 {
   "uri": ")" LED_ENDPOINT R"(",
-  "title": "LED",
+  "title": "Два светодиода",
   "menu": true,
   "element": [
     {
-      "name": "caption",
+      "name": "caption4",
       "type": "ACText",
-      "value": "BUILT-IN LED",
+      "value": "На D4",
       "style": "font-size:25px;text-align:center;",
       "posterior": "div"
     },
     {
-      "name": "onoff",
+      "name": "onoff4",
       "type": "ACButton",
-      "value": "ON"
+      "value": ")" ON R"("
+    },
+    {
+      "name": "caption2",
+      "type": "ACText",
+      "value": "На борту.",
+      "style": "font-size:25px;text-align:center;",
+      "posterior": "div"
+    },
+    {
+      "name": "onoff2",
+      "type": "ACButton",
+      "value": ")" ON R"("
     }
   ]
 }
@@ -33,38 +46,57 @@ const char LED_ONOFF[] PROGMEM = R"(
 // and a reference to the AutoConnectAux of the page rendered in the client
 // browser.
 void ledOnOff(AutoConnectButton& me, AutoConnectAux& ledOnOff) {
-  if (me.value == "ON") {
+  String currCaption = "";
+  int currentPin = 0;
+  // для реверсивных светодиодов этот флаг ставим в true. Это такие светодиоды, которые горят при нулевом значении на пине
+  bool isReverse = false; 
+  if(me.name == "onoff2") {
+    currCaption = "caption2";
+    currentPin  = LED_BUILTIN;
+    isReverse = true;
+  }
+  else if(me.name == "onoff4"){    
+    currCaption = "caption4";
+    currentPin  = LED_PIN_4;
+  }
+  // работа основная. если кнопка была названа ON, включаем и наоборот
+  if (me.value == ON) {
 
     // Since "ON" has been passed from the AutoConnectButton as `led`. Let the
     // LED turns on.
-    digitalWrite(LED_PIN_4, LED_ACTIVELEVEL);
+    bool LED_ACTIVELEVEL = true;
+    if(isReverse) LED_ACTIVELEVEL = false;
+    digitalWrite(currentPin, LED_ACTIVELEVEL);
 
     // Direct assignment to AutoConnectElement values is not reflected on the
     // web page; use the `response` function to update the value of the element
     // on the web page.
-    me.response("OFF");
+    me.response(OFF);
 
     // The `on` event handler attached to AutoConnectElements can override the
     // value and attributes of other elements placed on that AutoConnectAux page.
     // For example, a following statement changes the font color of the `caption`
     // element along with a LED blinking.
-    ledOnOff["caption"].response("style", "{\"color\":\"red\", \"font-weight\":\"bold\"}");
+    ledOnOff[currCaption].response("style", "{\"color\":\"red\", \"font-weight\":\"bold\"}");
   }
-  if (me.value == "OFF") {
-    // Since a value "OFF" has been passed from the AutoConnectButton as `led`.
-    // Let the LED turns off.
-    digitalWrite(LED_PIN_4, ~LED_ACTIVELEVEL & 0x1);
-    me.response("ON");
-    ledOnOff["caption"].response("style", "{\"color\":\"black\", \"font-weight\":\"normal\"}");
+  if (me.value == OFF) {
+    bool LED_ACTIVELEVEL = false;
+    if(isReverse) LED_ACTIVELEVEL = true;
+    digitalWrite(currentPin, LED_ACTIVELEVEL);
+    me.response(ON);
+    ledOnOff[currCaption].response("style", "{\"color\":\"black\", \"font-weight\":\"normal\"}");
   }
 }
 
-void buildInSetup(AutoConnect       &portal)
+// привязываем настройки
+void buildInSetup(AutoConnect &portal)
 {
     // Built-in LED port setting up
-  Serial.printf("LED port assignment GPIO #%d\n", LED_PIN_4);
+  Serial.printf("LEDs port assignment GPIOs ## %d, %d\n", LED_BUILTIN, LED_PIN_4);
   pinMode(LED_PIN_4, OUTPUT);
-  digitalWrite(LED_PIN_4, ~LED_ACTIVELEVEL & 0x1);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, true);// это реверсивный светодиод, ему true - выключает
+  digitalWrite(LED_PIN_4, false); 
 
   // связываем конфиг
 
@@ -76,7 +108,10 @@ void buildInSetup(AutoConnect       &portal)
 
   // The AutoConnectElement::on function allows the sketch to register an event
   // handler that interacts with the element individually.
-  AutoConnectButton& onOff = led["onoff"].as<AutoConnectButton>();
-  onOff.on(ledOnOff);
+  AutoConnectButton& onOff4 = led["onoff4"].as<AutoConnectButton>();
+  onOff4.on(ledOnOff);
+  // для реакции на кнопку 2
+    AutoConnectButton& onOff2 = led["onoff2"].as<AutoConnectButton>();
+  onOff2.on(ledOnOff);
   Serial.println("Access to blink the LED http://" + WiFi.localIP().toString() + LED_ENDPOINT);
 }
